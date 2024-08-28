22a7e8782aade43f59a8b06b57178e3509617abb
rutu-cyber <rutujadevkar@gmail.com>
2024-08-28 23:44:29 +0530

    template_literals

---
+++ 1_ES6_features/index.js
@@ -0,0 +1,11 @@
+// Template Literals
+const age= 20;
+const fullname= 'John Doe';
+
+// without string or template literals
+console.log("Hello Mr"+fullname+" your age is:"+age);
+
+// with template lietrals
+console.log(`Hello Mr ${fullname} your age is: ${age}`);
+
+
